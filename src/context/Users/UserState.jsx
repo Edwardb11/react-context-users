cebdf40d34c2334e80db2065e8925d09ab20f431
Edward Brito <edwardbrito11@hotmail.com>
2022-02-19 20:41:48 -0400

    logica de las funciones obteniendo datos de una API similar a jsomPLAHCEHOLDER

--- src/context/Users/UserState.jsx
+++ src/context/Users/UserState.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useReducer } from "react";
 import UserContext from "./UserContext";
 import UserReducer from "./UserReducer";
@@ -8,15 +9,26 @@ const UserState = () => {
     selectedUser: null,
   };
   const [state, dispatch] = useReducer(UserReducer, initialState);
-  const getUser = () => {};
-  const getProfile = () => {};
+  const getUser = async () => {
+    const res = await axios.get("https://reqres.in/api/users");
+    console.log(res);
+  };
+  const getProfile = async (id) => {
+    const res = await axios.get(`https://reqres.in/api/users/${id}`);
+    console.log(res);
+  };
   return (
     //   le pasamos en el provide como value a que pueden acceder los elementos hijos
     <UserContext.Provide
-      value={{ user: state.users, selectedUser: state.selectedUser, getUser,getProfile}}
+      value={{
+        user: state.users,
+        selectedUser: state.selectedUser,
+        getUser,
+        getProfile,
+      }}
     >
       {/* Cualquier componente que este dentro de UserState puede hacer uso de todo el estado definido  */}
-      {props.children} 
+      {props.children}
     </UserContext.Provide>
   );
 };
