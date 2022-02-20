8a431608e2b60bd2f39aac3d18aa2c869534a4ad
Edward Brito <edwardbrito11@hotmail.com>
2022-02-19 20:55:48 -0400

    utilizando contexto

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useContext } from "react";
+import UserContext from "../context/Users/UserContext";
 
 const UserList = () => {
-  return <div>UserList</div>;
+  // aqui accedo al contexto que quiero acceder
+  const { getUsers } = useContext(UserContext);
+  useEffect(() => {
+    getUsers();
+  }, []);
+  return <div></div>;
 };
 
 export default UserList;
